a5f3f1ab1f2c6cfbe608ab6680f27caa6aab3237
Jason Vanover <theykraveformeradio@gmail.com>
2026-03-23 14:10:06 -0400

    DEBUG JWT ENV

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const express = require("express")
 const app = express()
 
 // ... KEEP YOUR EXISTING SERVER CODE BELOW
+
+app.get("/debug-jwt", (req,res)=>{
+  res.json({
+    jwt: process.env.TKFM_JWT_SECRET || "missing"
+  })
+})
+
