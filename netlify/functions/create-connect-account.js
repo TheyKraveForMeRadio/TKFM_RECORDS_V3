b694faa24bd96139086e402ac89dd076af663e6a
Jason Vanover <theykraveformeradio@gmail.com>
2026-04-15 20:56:21 -0400

    auto payouts system

--- netlify/functions/create-connect-account.js
+++ netlify/functions/create-connect-account.js
@@ -1,11 +1,27 @@
 import Stripe from "stripe";
+import fetch from "node-fetch";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function handler(event) {
   try {
+    const { email } = JSON.parse(event.body);
+
     const account = await stripe.accounts.create({
       type: 'express'
+    });
+
+    // 🔥 SAVE ACCOUNT ID
+    await fetch(`${process.env.SUPABASE_URL}/rest/v1/referral_stats`, {
+      method: "PATCH",
+      headers: {
+        "apikey": process.env.SUPABASE_ANON_KEY,
+        "Authorization": `Bearer ${process.env.SUPABASE_ANON_KEY}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        stripe_account_id: account.id
+      })
     });
 
     const link = await stripe.accountLinks.create({
